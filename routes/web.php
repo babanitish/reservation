19fda511424b434231e184e9ac0231bd4f0e1dda
Aboubacar Toure <aboubacar212toure@gmail.com>
2022-02-13 19:53:32 +0100

    Controller Location +templates +routes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ArtistController;
 use App\Http\Controllers\TypeController;
 use App\Http\Controllers\LocalityController;
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\LocationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -35,3 +36,7 @@ Route::get('/locality/{id}', [LocalityController::class, 'show'])
 Route::get('/role', [RoleController::class, 'index'])->name('role_index');
 Route::get('/role/{id}', [RoleController::class, 'show'])
     ->where('id', '[0-9]+')->name('role_show');
+
+Route::get('location', [LocationController::class, 'index'])->name('location_index');
+Route::get('location/{id}', [LocationController::class, 'show'])
+    ->where('id', '[0-9]+')->name('location_show');
